e68ffb752512d0e75f4e464893c35dbe4dc2cea8
Oskari Päivinen <oskari.paivinen@gmail.com>
2014-04-10 14:50:01 +0300

    index update

--- src/ir_course/LuceneSearchApp.java
+++ src/ir_course/LuceneSearchApp.java
@@ -40,6 +40,7 @@ public class LuceneSearchApp {
 	
 	private Directory directory;
 	private Analyzer analyzer;
+	private int relevantDocCount = 0;
 	
 	private final int TASK_NUMBER = 19;
 	private final String[] queries = {
@@ -68,7 +69,13 @@ public class LuceneSearchApp {
 			    doc.add(new Field("abstract", entry.getAbstractText(), TextField.TYPE_STORED));
 			    doc.add(new Field("query", entry.getQuery(), TextField.TYPE_STORED));
 			    doc.add(new IntField("tasknumber", entry.getSearchTaskNumber(), IntField.TYPE_STORED));
-			    doc.add(new IntField("relevance", (entry.isRelevant() ? 1 : 0), IntField.TYPE_STORED));
+			    if (entry.isRelevant() && entry.getSearchTaskNumber() == TASK_NUMBER ){
+			    	doc.add(new IntField("relevance", 1 , IntField.TYPE_STORED));
+			    	this.relevantDocCount += 1;
+			    }
+			    else {
+			    	doc.add(new IntField("relevance", 0 , IntField.TYPE_STORED));
+			    }
 			    writer.addDocument(doc);
 			}
 			writer.close();    
@@ -83,153 +90,8 @@ public class LuceneSearchApp {
 	 * @return list of results
 	 * @throws IOException
 	 */
-	public List<String> search() throws IOException{
-		
-		List<String> results = new LinkedList<String>();
+	
 
-		//printQuery(inTitle, notInTitle, inDescription, notInDescription, startDate, endDate);
-		
-		// implement the Lucene search here
-		/*
-		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_42);
-		DirectoryReader ireader = DirectoryReader.open(FSDirectory.open(new File("index")));
-		IndexSearcher searcher = new IndexSearcher(ireader);
-		
-		BooleanQuery bq = new BooleanQuery();
-		QueryParser qparser = new QueryParser(Version.LUCENE_42, "title", analyzer);
-		if (inTitle != null){
-			for (final String title : inTitle){
-				Query query = null;
-				try {
-					query = qparser.parse(title);
-					bq.add(query, Occur.MUST);
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-				
-			}
-		}
-		if (notInTitle != null){
-			for (final String title : notInTitle){
-				Query query = null;
-				try {
-					query = qparser.parse(title);
-					bq.add(query, Occur.MUST_NOT);
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-				
-			}
-		}
-		qparser = new QueryParser(Version.LUCENE_42, "description", analyzer);
-		if (inDescription != null){
-			for (final String desc : inDescription){
-				Query query = null;
-				try {
-					query = qparser.parse(desc);
-					bq.add(query, Occur.MUST);
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-				
-			}
-		}
-		if (notInDescription != null){
-			for (final String desc : notInDescription){
-				Query query = null;
-				try {
-					query = qparser.parse(desc);
-					bq.add(query, Occur.MUST_NOT);
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-				
-			}
-		}
-		if (startDate!=null || endDate!=null){
-			long min=0;
-			long max=0;
-			if (startDate == null){
-				min = 0;
-			}else{
-				startDate += " 00:00";
-				SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-				try {
-					Date d = f.parse(startDate);
-					min = d.getTime();
-				} catch (java.text.ParseException e) {
-					e.printStackTrace();
-				}
-			}
-			if (endDate == null){
-				max = 0;
-			}else{
-				endDate += " 23:59";
-				SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-				try {
-					Date d = f.parse(endDate);
-					max = d.getTime();
-				} catch (java.text.ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			Query query=null;
-			if (min!=0 && max != 0){query = NumericRangeQuery.newLongRange("pubdate", min, max, true, true);}
-			else if (min == 0 && max != 0){query = NumericRangeQuery.newLongRange("pubdate", null, max, true, true);}
-			else if (min != 0 && max == 0){query = NumericRangeQuery.newLongRange("pubdate", min, null, true, true);}
-			bq.add(query, Occur.MUST);
-		}
-		ScoreDoc[] hits = searcher.search(bq, null, 1000).scoreDocs;
-		for (int i = 0; i < hits.length; i++){
-			Document hitdoc = searcher.doc(hits[i].doc);
-			results.add(hitdoc.get("title"));
-		}
-		*/
-		return results;
-	}
-	
-	public void printQuery() {
-		/*System.out.print("Search (");
-		if (inTitle != null) {
-			System.out.print("in title: "+inTitle);
-			if (notInTitle != null || inDescription != null || notInDescription != null || startDate != null || endDate != null)
-				System.out.print("; ");
-		}
-		if (notInTitle != null) {
-			System.out.print("not in title: "+notInTitle);
-			if (inDescription != null || notInDescription != null || startDate != null || endDate != null)
-				System.out.print("; ");
-		}
-		if (inDescription != null) {
-			System.out.print("in description: "+inDescription);
-			if (notInDescription != null || startDate != null || endDate != null)
-				System.out.print("; ");
-		}
-		if (notInDescription != null) {
-			System.out.print("not in description: "+notInDescription);
-			if (startDate != null || endDate != null)
-				System.out.print("; ");
-		}
-		if (startDate != null) {
-			System.out.print("startDate: "+startDate);
-			if (endDate != null)
-				System.out.print("; ");
-		}
-		if (endDate != null)
-			System.out.print("endDate: "+endDate);
-		System.out.println("):");
-	}
-	
-	public void printResults(List<String> results) {
-		if (results.size() > 0) {
-			Collections.sort(results);
-			for (int i=0; i<results.size(); i++)
-				System.out.println(" " + (i+1) + ". " + results.get(i));
-		}
-		else
-			System.out.println(" no results");*/
-	}
 	
 	public static void main(String[] args) throws IOException {
 		if (args.length > 0) {
@@ -240,9 +102,6 @@ public class LuceneSearchApp {
 			List<DocumentInCollection> docs = parser.getDocuments();
 			engine.index(docs);
 
-			// TODO Create searchers
-			
-			engine.search();
 		}
 		else
 			System.out.println("ERROR: the path of a RSS Feed file has to be passed as a command line argument.");
